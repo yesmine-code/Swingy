687c5f0b4e74ee87c2b7e1be4692be85abd414e4
BESBES Yesmine <yesmine.besbes@vinci-energies.net>
2023-01-10 10:51:27 +0100

    ImagePanel class added

---
+++ src/main/java/org/yesmine/view/swing/ImagePanel.java
@@ -0,0 +1,30 @@
+package org.yesmine.view.swing;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel{
+
+    private BufferedImage image;
+
+    public ImagePanel(String filePath) {
+        try {
+            image = ImageIO.read(new File(filePath));
+        } catch (IOException ex) {
+            // handle exception...
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
+    }
+
+}
